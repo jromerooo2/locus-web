b6378a5176d4473dc83fb720e467a67c47e460c7
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-08-25 13:59:38 -0600

    loading animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
 gsap.to("#logo", {
-    duration: 1, y: -150
+    duration: 1, y: -200
 });
 
 gsap.to("#info-text", {
     duration: 1,
 })
+
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
 
