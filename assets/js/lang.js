0e95b1881d09fc0c49b5b5db45478eb9af8d00d3
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-09-14 08:27:26 -0600

    updates: not more user lang bugs and added team info

--- assets/js/lang.js
+++
@@ -1,8 +0,0 @@
-//detect browsers lang and redirect based on it
- var lang = navigator.language || navigator.browserLanguage;
-
-  if (lang.indexOf('en') > -1) {
-  document.location.href = 'usindex.html';
-  } else if(lang.indexOf('fr') > -1){
-  document.location.href = 'frindex.html';
-  }
