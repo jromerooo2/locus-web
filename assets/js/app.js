e21559b76a33a7e866039b200e99ded444b49fb4
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-09-20 12:47:02 -0600

    testimonial section and some timing fixing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,66 @@
-document.getElementById("logo").style.display="none";var btn=$("#button");$(window).scroll(function(){$(window).scrollTop()>300?btn.addClass("show"):btn.removeClass("show")}),btn.on("click",function(e){e.preventDefault(),$("html, body").animate({scrollTop:0},"300")}),$("#btn-learn").on("click",e=>{e.preventDefault(),$("html, body").animate({scrollTop:$("#info").offset().top},300)});var animated=!1;function toPX(e){return parseFloat(e)/100*(/vh/gi.test(e)?window.innerHeight:window.innerWidth)}$(document).on("scroll",function(){screen.width<=900&&$(this).scrollTop()>=$("#troll").position().top&&!animated&&(animated=!0,$("#exampleModalCenter").modal("show"),$("#btn-close").on("click",()=>{$("#exampleModalCenter").modal("hide")}))}),$(".selectpicker").change(function(){switch($(this).val()){case"1":window.location.replace("index.html");break;case"2":window.location.replace("usindex.html");break;case"3":window.location.replace("frindex.html")}}),$(window).on("load",function(){setTimeout(function(){$(".loader-wrapper").fadeOut("slow"),document.getElementById("logo").style.display="block",gsap.to("#logo",{duration:1,y:toPX("-26vh")}),gsap.to("#info-text",{duration:1,y:"-10%"})},100)});const navbar=$("#navbar-phone"),toggler=$(".navbar-toggler");$(window).scroll(()=>{screen.width<=900&&(navbar.addClass("bg-purple"),toggler.addClass("text-white")),clearTimeout($.data(this,"scrollTimer")),$.data(this,"scrollTimer",setTimeout(function(){screen.width<=900&&(navbar.removeClass("bg-purple"),toggler.removeClass("text-white"))},450))});
+document.getElementById("logo").style.display = "none";
+var btn = $("#button");
+$(window).scroll(function () {
+    $(window).scrollTop() > 300 ? btn.addClass("show") : btn.removeClass("show");
+}),
+    btn.on("click", function (e) {
+        e.preventDefault(), $("html, body").animate({ scrollTop: 0 }, "300");
+    }),
+    $("#btn-learn").on("click", (e) => {
+        e.preventDefault(), $("html, body").animate({ scrollTop: $("#info").offset().top }, 300);
+    });
+var animated = !1;
+function toPX(e) {
+    return (parseFloat(e) / 100) * (/vh/gi.test(e) ? window.innerHeight : window.innerWidth);
+}
+    $(".selectpicker").change(function () {
+        switch ($(this).val()) {
+            case "1":
+                window.location.replace("index.html");
+                break;
+            case "2":
+                window.location.replace("usindex.html");
+                break;
+            case "3":
+                window.location.replace("frindex.html");
+        }
+    }),
+    $(window).on("load", function () {
+        setTimeout(function () {
+            $(".loader-wrapper").fadeOut("slow"), (document.getElementById("logo").style.display = "block"), gsap.to("#logo", { duration: 1, y: toPX("-26vh") }), gsap.to("#info-text", { duration: 1, y: "-10%" });
+            if (screen.width <= 900) {
+                gsap.to(".row-cards2", {
+                    x:"-520%",
+                    yoyo:true,
+                    repeat: -1, 
+                    duration:9 
+                })
+            }else{
+                gsap.to(".row-cards2", {
+                    x:"-50%",
+                    yoyo:true,
+                    repeat: -1, 
+                    duration:12 
+                })
+            }
+        }, 100);
+    });
+const navbar = $("#navbar-phone"),
+    toggler = $(".navbar-toggler");
+$(window).scroll(() => {
+    screen.width <= 900 && (navbar.addClass("bg-purple"), toggler.addClass("text-white")),
+        clearTimeout($.data(this, "scrollTimer")),
+        $.data(
+            this,
+            "scrollTimer",
+            setTimeout(function () {
+                screen.width <= 900 && (navbar.removeClass("bg-purple"), toggler.removeClass("text-white"));
+            }, 450)
+        );
+});
+
+
+
     /*
     :  .         .::       ':        ::        ::       :.y. :     
     .: :::       :" ::       :        ::      .:'':      .yyy.':    
